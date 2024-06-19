c996108bc20cd0c569e1f8a1a59c19ee55eed5e4
Somsuvro1998 <Palz.somsuvro@gmail.com>
2024-06-19 22:12:08 +0530

    commit1

---
+++ src/test/java/demo/Demonstration.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class Demonstration {
+    @Test
+	public void hello() {
+		System.out.println("Hello");
+		System.out.println("hi");
+		System.out.println("Goodbye");
+	}
+}
---
+++ src/test/java/demo/Demonstration1.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class Demonstration1 {
+
+	public static void main(String[] args) {
+System.out.println("hello");
+System.out.println("hi");
+System.out.println("Goodbye");
+	}
+
+}
